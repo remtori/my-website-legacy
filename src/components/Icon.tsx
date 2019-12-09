ac5cfb8fa7e969e8f22a4cea205533b5086676b4
Remtori <lqv.remtori@gmail.com>
2019-12-09 10:59:36 +0700

    Stopped working on this

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -4,29 +4,32 @@ import { faUser } from "@fortawesome/free-solid-svg-icons/faUser";
 import { faClock } from "@fortawesome/free-solid-svg-icons/faClock";
 import { faTags } from "@fortawesome/free-solid-svg-icons/faTags";
 import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
+import { faAddressCard } from "@fortawesome/free-solid-svg-icons/faAddressCard";
+import { faHome } from "@fortawesome/free-solid-svg-icons/faHome";
+import { faTasks } from "@fortawesome/free-solid-svg-icons/faTasks";
+import { faCommentAlt } from "@fortawesome/free-solid-svg-icons/faCommentAlt";
 
 library.add(
     faUser, faClock, faTags,
     faGithub,
+    faHome, faTasks, faAddressCard,
+    faCommentAlt,
 );
 
 const Icon = ({ icon: iconName, class: className }: {[key: string]: string | any}) => {
 
-    const { abstract } = icon(
+    const { abstract: [ ele ] } = icon(
         iconName,
         { classes: className },
     );
 
-    const ele = abstract[0];
-    const child = ele.children && ele.children[0];
-
     return h(
         ele.tag,
         { ...ele.attributes },
-        child && h(
-            child.tag,
-            { ...child.attributes },
-        ),
+        ele.children && ele.children.map(c => h(
+            c.tag,
+            { ...c.attributes },
+        )),
     );
 };
 
